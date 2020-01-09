6b1f0ffe25c7be2915bb71d9c7a55996ade5a282
80003996 <627460581@qq.com>
2020-01-09 18:46:56 +0800

    文档 mysql 索引

--- bootdemo/src/main/java/com/java8/App.java
+++ bootdemo/src/main/java/com/java8/App.java
@@ -1,8 +1,10 @@
 package com.java8;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
@@ -14,17 +16,16 @@ import java.util.stream.Collectors;
 public class App {
     public static void main(String[] args) {
 
-        List<String> list = Lists.newArrayList();
-        list.add("zh");
-        list.add("qian");
-        list.add("sun");
-        list.add("li");
-        list.add("zhou");
-        List<String> li = list.stream().filter(x -> x.equals("li")).collect(Collectors.toList());
-        System.out.println("list++++++++++++++++");
-        list.forEach(System.out::println);
-        System.out.println("li================");
-        li.forEach(System.out::println);
+        Person person = Person.builder().age(11).name("zhangsan").build();
+        Person person1 = Person.builder().age(12).name("lisi").build();
+        Person person2 = Person.builder().age(13).name("wangwu").build();
+        Person person3 = Person.builder().age(14).name("zhaoliu").build();
+        Person person4 = Person.builder().age(15).name("tianqi").build();
+
+        List<Person> list = Lists.newArrayList(person, person1, person2, person3, person4);
+
+        Map<String, Person> stringMap = list.stream().collect(Collectors.toMap(Person::getName, y -> y));
+        System.out.println(stringMap);
 
     }
 
