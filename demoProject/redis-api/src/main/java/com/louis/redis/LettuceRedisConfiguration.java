b24d29833a19ef747e09d1f5d8505904ff4ac4c9
80003996 <627460581@qq.com>
2020-03-06 15:10:20 +0800

    Merge branch 'master' of https://gitee.com/Eric125/boot-mybatis
    # Conflicts:
    #	readme.md

---
+++ demoProject/redis-api/src/main/java/com/louis/redis/LettuceRedisConfiguration.java
@@ -0,0 +1,48 @@
+package com.louis.redis;
+
+import com.sf.framework.cacheproxy.redis.RedisCache;
+import com.sf.framework.cacheproxy.redis.RedisConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+/**
+ * @author louis
+ * <p>
+ * Date: 2020/2/27
+ * Description:
+ */
+@Configuration
+public class LettuceRedisConfiguration {
+
+
+    @Value("${mk.autocache.servers}")
+    private String redisServers;
+
+    @Value("${mk.autocache.password}")
+    private String password;
+
+
+    @Value("${mk.autocache.masters}")
+    private String masters;
+
+    @Bean
+    public RedisCache redisCache() {
+        return new RedisCache(redisConfig());
+    }
+
+
+
+    @Bean
+    public RedisConfig redisConfig() {
+        String realServers = String.join(",", redisServers.split(" "));
+        return new RedisConfig()
+                .setServers(realServers)
+                .setPassword(password)
+                .setMasters(masters)
+                .setTtl(60 * 60*2);
+
+
+    }
+}
---
+++ demoProject/redis-api/src/main/java/com/louis/redis/WmoRedisCache.java
@@ -0,0 +1,88 @@
+package com.louis.redis;
+
+import com.sf.framework.cacheproxy.CPException;
+import com.sf.framework.cacheproxy.redis.*;
+import io.lettuce.core.cluster.api.sync.RedisClusterCommands;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.util.ReflectionUtils;
+
+import java.lang.reflect.Method;
+import java.util.*;
+
+/**
+ * @author louis
+ * <p>
+ * Date: 2020/3/2
+ * Description:
+ */
+@Service
+public class WmoRedisCache  {
+
+    @Autowired
+    private RedisCache redisCache;
+
+
+    /**
+     * 通过反射的方式获取 RedisCache中的RedisClusterCommands对象
+     * @param pattern
+     * @return
+     */
+    @SuppressWarnings("unchecked")
+    private RedisClusterCommands<String, byte[]> wmoCommands(String pattern) {
+        Method method=ReflectionUtils.findMethod(redisCache.getClass(), "commands",String.class);
+       return Optional.ofNullable(method).map(m -> {
+            ReflectionUtils.makeAccessible(method);
+            Object obj = ReflectionUtils.invokeMethod(method, redisCache, pattern);
+            return (RedisClusterCommands) Optional.ofNullable(obj).orElse(null);
+        }).orElse(null);
+
+
+    }
+
+    public Set<String> keys(String pattern) {
+        checkKey(pattern);
+        List<String> keys = Optional
+                .ofNullable(wmoCommands(pattern))
+                .map(commands -> commands.keys(pattern))
+                .orElse(new ArrayList<>());
+
+        return new HashSet<>(keys);
+    }
+
+    /**
+     *
+     * @param key
+     * @return
+     */
+    public Object getIgnoreType(String key) {
+        String type = redisCache.type(key);
+
+        if (type.equals("hash")) {
+            return redisCache.hgetAll(key);
+        } else if (type.equals("set")) {
+            return redisCache.smembers(key);
+
+        } else if (type.equals("list")) {
+            return redisCache.members(key);
+        } else {
+            return redisCache.get(key);
+        }
+    }
+
+
+
+
+    private void checkKey(String key) {
+        this.assertIfNull(key, "key is null");
+    }
+    private void assertIfNull(Object obj, String msg) {
+        if (obj == null) {
+            throw new CPException(msg);
+        }
+    }
+
+
+
+
+}
