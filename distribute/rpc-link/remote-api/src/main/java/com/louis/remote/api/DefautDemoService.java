4295cebeeb42bcadc686cbd4b295b20dc31e2bf9
jun.liu <627460581@qq.com>
2020-07-12 20:02:10 +0800

    dubbo rest 请求

--- distribute/rpc-link/remote-api/src/main/java/com/louis/remote/api/DefautDemoService.java
+++ distribute/rpc-link/remote-api/src/main/java/com/louis/remote/api/DefautDemoService.java
@@ -1,7 +1,6 @@
 package com.louis.remote.api;
 
 
-import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Map;
@@ -12,14 +11,14 @@ import java.util.Map;
  * Date: 2019/9/16
  * Description:
  */
-//@Path("/default")
-//@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_HTML, MediaType.TEXT_PLAIN, MediaType.TEXT_XML})
-//@Produces({MediaType.APPLICATION_JSON + "; " + MediaType.CHARSET_PARAMETER + "=UTF-8", MediaType.TEXT_XML + "; " + MediaType.CHARSET_PARAMETER + "=UTF-8"})
+@Path("/default")
+@Consumes({MediaType.APPLICATION_JSON, MediaType.TEXT_HTML, MediaType.TEXT_PLAIN, MediaType.TEXT_XML})
+@Produces({MediaType.APPLICATION_JSON })
 public interface DefautDemoService {
 
 
-    //    @GET
-//    @Path("selectByOpenId/{name}")
-//    String sayHello(@PathParam("name") String name);
-    Map<String, Object> sayHello(String name);
+        @GET
+    @Path("selectByOpenId/{name}")
+    Map<String, Object> sayHello(@PathParam("name") String name);
+//    Map<String, Object> sayHello(String name);
 }
