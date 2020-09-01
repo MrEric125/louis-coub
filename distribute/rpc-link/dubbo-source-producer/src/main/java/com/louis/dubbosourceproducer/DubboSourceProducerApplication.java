cd5b8dcdce9cf0c92a6d0acfe01f50d55306789c
jun.liu <627460581@qq.com>
2020-09-01 17:27:07 +0800

    dubbo 功能独立出来，

--- distribute/rpc-link/dubbo-source-producer/src/main/java/com/louis/dubbosourceproducer/DubboSourceProducerApplication.java
+++ distribute/rpc-link/dubbo-source-producer/src/main/java/com/louis/dubbosourceproducer/DubboSourceProducerApplication.java
@@ -1,13 +1,29 @@
 package com.louis.dubbosourceproducer;
 
+import com.google.common.collect.Maps;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/test")
 public class DubboSourceProducerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DubboSourceProducerApplication.class, args);
+    }
+
+    @RequestMapping("/test")
+    public Map<String,Object> testResult() {
+        HashMap<String, Object> ha = Maps.newHashMap();
+        ha.put("zhangsan", "zhansss");
+        return ha;
+
     }
 
 }
