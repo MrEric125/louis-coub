789e0656ef530ec25050d28bb1ff9799e4239d74
jun.liu <jun.liu@dmall.com>
2020-12-02 20:27:30 +0800

    DubboSpi

--- distribute/rpc-link/remote-api/src/main/java/com/louis/remote/api/Robots.java
+++ distribute/rpc-link/remote-api/src/main/java/com/louis/remote/api/Robots.java
@@ -1,10 +1,13 @@
 package com.louis.remote.api;
 
+import org.apache.dubbo.common.extension.SPI;
+
 /**
  * @author jun.liu
  * @date created on 2020/8/12
  * description:
  */
+@SPI()
 public interface Robots {
 
     public void sayHello();
