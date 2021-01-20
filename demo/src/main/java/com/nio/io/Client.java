4824a53bfde6e9431ef5121bdafb38e5f86e62c1
jun.liu <627460581@qq.com>
2021-01-20 23:02:35 +0800

    bio demo

---
+++ demo/src/main/java/com/nio/io/Client.java
@@ -0,0 +1,26 @@
+package com.nio.io;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.Socket;
+
+/**
+ * @author Louis
+ * @date created on 2021/1/20
+ * description:
+ * todo 客户端和服务端总共有多少个socket,总共会有多少个地方阻塞
+ *
+ */
+public class Client {
+
+    public static void main(String[] args) {
+        try {
+            Socket socket = new Socket("localhost", 8080);
+            //连完之后可以不发送数据的
+            socket.getOutputStream().write("121212".getBytes());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ demo/src/main/java/com/nio/readme.md
@@ -0,0 +1,28 @@
+
+BIO ： 同步阻塞IO
+    java.io包下的列
+    InputStream 
+    OutputStream
+    Reader
+    Writer
+    
+    网络编程
+        ServerSocker
+        Socket
+    阻塞： read方法 accept 方法，阻塞式的
+    
+NIO: 同步非阻塞
+    java.nio   包的类
+    Buffer
+        ByteBuffer
+        CharBuffer
+    Channel
+        FileChannel
+    
+    网络编程
+        ServerSocketChannel
+        SocketChannel
+        Selector
+    非阻塞： read 方法，accept方法  可以设置阻塞还是非阻塞
+    AsynchronousSocketChannel
+                       
