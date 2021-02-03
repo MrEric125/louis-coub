273e093b7354014f06bbeb05fd3f8391e394cf7a
jun.liu <627460581@qq.com>
2021-02-03 22:14:34 +0800

    spring boot 降低版本优化

--- demoProject/minaproject/src/main/java/com/louis/TreeNode.java
+++ demoProject/minaproject/src/main/java/com/louis/TreeNode.java
@@ -1,10 +1,21 @@
 package com.louis;
 
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 /**
  * @author jun.liu
  * @date created on 2020/11/17
  * description:
  */
 public class TreeNode {
+
+    Lock lock = new ReentrantLock();
+
+    public void method(){
+        lock.unlock();
+
+    }
+
 
 }
--- demoProject/minaproject/src/main/java/com/louis/minaProject/config/RedisConfiguration.java
+++ demoProject/minaproject/src/main/java/com/louis/minaProject/config/RedisConfiguration.java
@@ -85,10 +85,10 @@ public class RedisConfiguration extends CachingConfigurerSupport {
         // 值采用json序列化
         //使用StringRedisSerializer来序列化和反序列化redis的key值
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(RedisSerializer.byteArray());
+        template.setValueSerializer(RedisSerializer.json());
         // 设置hash key 和value序列化模式
         template.setHashKeySerializer(new StringRedisSerializer());
-        template.setHashValueSerializer(RedisSerializer.byteArray());
+        template.setHashValueSerializer(RedisSerializer.json());
         template.afterPropertiesSet();
         return template;
     }
