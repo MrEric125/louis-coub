3f1ae4a4674711a5457593fa71591bd2e1056df1
jun.liu <627460581@qq.com>
2021-02-05 23:11:59 +0800

    netty

---
+++ coub-netty/src/main/java/louis/coub/netty/nio/NioBufferDemo.java
@@ -0,0 +1,9 @@
+package louis.coub.netty.nio;
+
+/**
+ * @author Louis
+ * @date created on 2021/2/5
+ * description:
+ */
+public class NioBufferDemo {
+}
--- demoProject/minaproject/src/main/java/com/louis/minaProject/config/RedisConfiguration.java
+++ demoProject/minaproject/src/main/java/com/louis/minaProject/config/RedisConfiguration.java
@@ -15,9 +15,7 @@ import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.cache.RedisCacheWriter;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.*;
 
 import java.io.Serializable;
