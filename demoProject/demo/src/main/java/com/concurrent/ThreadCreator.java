3519c44715ba5a22b87320520aabadc748d80163
Louis <627460581@qq.com>
2021-03-10 09:24:13 +0800

    gradle --> maven

--- demoProject/demo/src/main/java/com/concurrent/ThreadCreator.java
+++ demoProject/demo/src/main/java/com/concurrent/ThreadCreator.java
@@ -3,13 +3,54 @@ package com.concurrent;
 public class ThreadCreator {
 
 
-    public static void main(String[] args) {
-        Thread thread = new Thread(() -> {
-            System.out.println("thread test");
+    public static void main(String[] args) throws InterruptedException {
+        Thread threadA = new Thread(() -> {
+            try {
+                synchronized (resourceA) {
+                    System.out.println("thread A get resource A lock");
 
+                    synchronized (resourceB) {
+                        System.out.println("thread A get resource B lock");
+
+                        System.out.println("thread Arelease resource A lock");
+
+                        resourceA.wait();
+                    }
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         });
-        thread.start();
 
+        Thread threadB=new Thread(()->{
+            try {
+                Thread.sleep(1000);
+                synchronized (resourceA) {
+                    System.out.println("thread B get resource A lock");
+
+                    System.out.println("thread B try get resourceB lock..");
+
+                    synchronized (resourceB) {
+                        System.out.println("thread B get resource B lock");
+
+                        System.out.println("thread B release resource A lock");
+                    }
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+
+            }
+        });
+        threadA.start();
+        threadB.start();
+        threadA.join();
+        threadB.join();
+        System.out.println("main over");
     }
+
+    private  static final Object resourceA = new Object();
+    private static final Object resourceB = new Object();
+
+
 
 }
