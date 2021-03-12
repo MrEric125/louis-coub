8083299fbdd5430275e1ffea5129fc0abc20f266
Louis <627460581@qq.com>
2021-03-12 14:51:02 +0800

    tt

---
+++ demoProject/coub-auto-deploy/src/main/java/com/coub/louis/DeployApp.java
@@ -0,0 +1,13 @@
+package com.coub.louis;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeployApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DeployApp.class, args);
+    }
+}
---
+++ demoProject/demo/src/main/java/com/concurrent/inpractice/chapter5/TestHarness.java
@@ -0,0 +1,36 @@
+package com.concurrent.inpractice.chapter5;
+
+import java.util.concurrent.CountDownLatch;
+
+public class TestHarness {
+
+    public long timeTasks(int nThreads, final Runnable task) throws InterruptedException {
+        final CountDownLatch startGate = new CountDownLatch(1);
+        final CountDownLatch endGate = new CountDownLatch(nThreads);
+
+        for (int i = 0; i < nThreads; i++) {
+            Thread thread = new Thread(() -> {
+                try {
+                    startGate.await();
+                    try {
+                        task.run();
+
+                    } finally {
+                        endGate.countDown();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            });
+            thread.start();
+
+        }
+        long start = System.nanoTime();
+        startGate.countDown();
+        endGate.await();
+        long end = System.nanoTime();
+        return end - start;
+
+    }
+
+}
