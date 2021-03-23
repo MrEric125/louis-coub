6956cbf0fdf8f3924a2bfad9c5a95161a84a4197
jun.liu <627460581@qq.com>
2021-03-23 22:42:18 +0800

    jenkins

---
+++ demoProject/coub-auto-deploy/src/main/java/com/coub/louis/deploy/JenkinsProperties.java
@@ -0,0 +1,19 @@
+package com.coub.louis.deploy;
+
+import lombok.Data;
+
+/**
+ * @author Louis
+ * @date created on 2021/3/21
+ * description:
+ */
+@Data
+public class JenkinsProperties {
+
+    private String serverUri;
+
+    private String username;
+
+    private String password;
+
+}
