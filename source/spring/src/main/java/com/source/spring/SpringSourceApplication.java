86ee9bbffe0b36752ad7b3fcab69d71926760176
jun.liu <jun.liu@dmall.com>
2021-05-19 13:52:41 +0800

    pom 依赖升级

--- source/spring/src/main/java/com/source/spring/SpringSourceApplication.java
+++ source/spring/src/main/java/com/source/spring/SpringSourceApplication.java
@@ -1,17 +1,15 @@
 package com.source.spring;
 
+import com.alibaba.fastjson.JSON;
 import com.google.common.cache.Cache;
 import com.google.common.cache.CacheBuilder;
 import com.louis.common.common.HttpResult;
 import org.springframework.beans.BeansException;
-import org.springframework.boot.Banner;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.SpringBootConfiguration;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -26,11 +24,7 @@ import java.util.concurrent.TimeUnit;
  * @ComponentScan只是spring mvc xml功能迁移而已，扫描当前包下的所有带配置类
  */
 @RestController
-//@SpringBootApplication
-@SpringBootConfiguration
-@EnableAutoConfiguration
-//@ComponentScan(excludeFilters = { @ComponentScan.Filter(type = FilterType.CUSTOM, classes = TypeExcludeFilter.class),
-//        @ComponentScan.Filter(type = FilterType.CUSTOM, classes = AutoConfigurationExcludeFilter.class) })
+@SpringBootApplication
 public class SpringSourceApplication implements CommandLineRunner, ApplicationContextAware {
 
     Cache<String, String> myCache = CacheBuilder.newBuilder().concurrencyLevel(4).expireAfterWrite(10, TimeUnit.DAYS).maximumSize(10000).build();
@@ -39,7 +33,6 @@ public class SpringSourceApplication implements CommandLineRunner, ApplicationCo
 
     public static void main(String[] args) {
         SpringApplication sa = new SpringApplication(SpringSourceApplication.class);
-        sa.setBannerMode(Banner.Mode.OFF);
 
         sa.run();
 //        run.publishEvent(new MyEvent(new Object()));
@@ -59,7 +52,7 @@ public class SpringSourceApplication implements CommandLineRunner, ApplicationCo
     //todo 在SpringApplication.run 运行完成之前执行，特殊逻辑,实现原理是什么样子的？？？
     @Override
     public void run(String... args) throws Exception {
-        System.out.println(args);
+        System.out.println(JSON.toJSONString(args));
     }
 
     @Override
