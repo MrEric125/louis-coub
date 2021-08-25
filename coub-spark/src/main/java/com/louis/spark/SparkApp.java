2240302a3da9100fff63a99386f58b4cb3af0b30
jun.liu <jun.liu@dmall.com>
2021-08-25 14:08:20 +0800

    数据细分

---
+++ coub-spark/src/main/java/com/louis/spark/SparkApp.java
@@ -0,0 +1,13 @@
+package com.louis.spark;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkApp.class, args);
+    }
+}
---
+++ coub-spark/src/test/java/SparkTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class SparkTest {
+
+    @Test
+    public void test() {
+
+    }
+}
