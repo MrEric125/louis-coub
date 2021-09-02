27700824ed91697ed1b8f2b5897dff245ca0e3bd
Louis <627460581@qq.com>
2021-09-02 19:44:19 +0800

    spark run success

--- coub-spark/src/main/java/com/louis/spark/SparkApp.java
+++ coub-spark/src/main/java/com/louis/spark/SparkApp.java
@@ -1,6 +1,7 @@
 package com.louis.spark;
 
 
+import org.apache.commons.collections.CollectionUtils;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
@@ -9,18 +10,24 @@ import scala.Tuple2;
 
 import java.util.Arrays;
 import java.util.Iterator;
+import java.util.List;
 
 //@SpringBootApplication
 public class SparkApp {
 
     public static void main(String[] args) {
-        String inputFile = "readme.md";
-        String outputFile = "outputFile.txt";
+        String inputFile = "/Users/louis/workspace/louis/louis-coub/coub-spark/src/main/resources/readme.md";
+        String outputFile = "/Users/louis/workspace/louis/louis-coub/coub-spark/src/main/resources/outputFile.txt";
         SparkConf sparkConf = new SparkConf().setMaster("local").setAppName("My Spark");
-        JavaSparkContext jsc = new JavaSparkContext();
+        JavaSparkContext jsc = new JavaSparkContext(sparkConf);
+
         JavaRDD<String> stringJavaRDD = jsc.textFile(inputFile);
 
-        JavaRDD<String> words = stringJavaRDD.flatMap(s -> (Iterator<String>) Arrays.asList(s.split(" ")));
+        JavaRDD<String> words = stringJavaRDD.flatMap(s -> {
+            List<String> tList = Arrays.asList(s.split(" "));
+            return tList.iterator();
+
+        });
         JavaPairRDD<String, Integer> counts = words.mapToPair(s -> new Tuple2<>(s, 1)).reduceByKey(Integer::sum);
 
         counts.saveAsTextFile(outputFile);
