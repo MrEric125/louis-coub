4dd43010da715208894e0eca8dfbc07b8204a69e
jun.liu <jun.liu@dmall.com>
2021-09-06 14:46:54 +0800

    tt

--- demoProject/demo/src/main/java/com/spring/transaction/Tb1Service.java
+++ demoProject/demo/src/main/java/com/spring/transaction/Tb1Service.java
@@ -1,11 +1,12 @@
 package com.spring.transaction;
 
+import org.springframework.aop.framework.AopContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.task.TaskExecutor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import javax.xml.crypto.Data;
+import java.io.File;
 import java.util.Date;
 import java.util.Optional;
 
@@ -27,10 +28,11 @@ public class Tb1Service {
 
     @Transactional
     public Tb1 save(Tb1 tb1) {
-        return tb1Repository.save(tb1);
+        Tb1 save = tb1Repository.save(tb1);
+        throw new RuntimeException("save");
     }
 
-    @Transactional
+    @Transactional(rollbackFor = Exception.class)
     public Tb1 update(Long id) throws Exception {
         Optional<Tb1> byId = tb1Repository.findById(id);
 
@@ -38,26 +40,27 @@ public class Tb1Service {
 
 
         tb1.setName(new Date().toString());
-        Tb1 save = tb1Repository.save(tb1);
-        taskExecutor.execute(() -> {
-            Tb1 save2=selectById(save.getId());
-            save2.setName(new Date().toString());
 
-//            try {
+//        try {
+//            ((Tb1Service) AopContext.currentProxy()).save(tb1);
+//        } catch (Exception e) {
+//
+//        }
 
-                tb1Repository.save(save2);
-//                throw new Exception("dd error");
-//            } catch (Exception e) {
-//                e.printStackTrace();
-//            }
-        });
-        if (save != null) {
-            throw new Exception("dd");
-        }
-        return save;
+
+
+        new Thread(()->{
+            Tb1 tb2 = tb1;
+            tb2.setName(new Date().toString());
+            tb1Repository.save(tb2);
+            throw new RuntimeException("runtime");
+        }).start();
+        Thread.sleep(2000);
+        tb1Repository.save(tb1);
+        return null;
     }
 
-//    @Transactional
+    @Transactional
     public Tb1 selectById(Long id) {
         Tb1 save2 = tb1Repository.findById(id).get();
         return save2;
