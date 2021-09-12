6d19c7a1c6d949f16cb3f878f2f169c565a8667d
Louis <627460581@qq.com>
2021-09-12 23:34:04 +0800

    dubbo admin

--- distribute/rpc-link/dubbo-source-consumer/src/main/java/com/louis/dubbosourceconsumer/ZkClientController.java
+++ distribute/rpc-link/dubbo-source-consumer/src/main/java/com/louis/dubbosourceconsumer/ZkClientController.java
@@ -1,9 +1,14 @@
 package com.louis.dubbosourceconsumer;
 
+import com.google.common.collect.Lists;
 import com.louis.common.common.HttpResult;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.CreateMode;
 import org.apache.zookeeper.KeeperException;
+import org.apache.zookeeper.ZKUtil;
 import org.apache.zookeeper.ZooKeeper;
+import org.apache.zookeeper.data.ACL;
+import org.apache.zookeeper.data.Id;
 import org.apache.zookeeper.data.Stat;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -47,7 +52,7 @@ public class ZkClientController {
     }
 
 
-    @GetMapping("/path")
+    @GetMapping("/getPath")
     public HttpResult getStat(String path) throws KeeperException, InterruptedException, IOException {
 
 
@@ -67,6 +72,21 @@ public class ZkClientController {
         map.put("aVersion", stat.getAversion());
         map.put("version", stat.getVersion());
         return HttpResult.ok(map);
+    }
+
+    @GetMapping("/putData")
+    public HttpResult putData(String path,String data) throws KeeperException, InterruptedException {
+        Stat exists = zkClient.exists(path, Boolean.TRUE);
+
+        if (exists != null) {
+            Stat stat = zkClient.setData(path, data.getBytes(), 0);
+            return HttpResult.ok(stat);
+        }
+        ACL acl = new ACL();
+        acl.setPerms(0);
+        acl.setId(new Id("zhangsan","zhangsan"));
+        String s = zkClient.create(path, data.getBytes(), Lists.newArrayList(acl), CreateMode.EPHEMERAL);
+        return HttpResult.ok(s);
 
     }
 
