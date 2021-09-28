4be5ef9ae0f350e24475425507c0453b1765508d
Louis <627460581@qq.com>
2021-09-28 23:10:20 +0800

    事务日志级别

--- source/spring/src/main/java/com/source/spring/trans/TransactionConfig.java
+++ source/spring/src/main/java/com/source/spring/trans/TransactionConfig.java
@@ -8,6 +8,17 @@ package com.source.spring.trans;
  * config code 模式
  * {@link org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration}
  *
+ * 事务中几个关键的 类
+ * {@link org.springframework.transaction.interceptor.TransactionInterceptor}
+ *
+ * {@link org.springframework.transaction.interceptor.TransactionProxyFactoryBean}
+ *
+ * {@link org.springframework.transaction.interceptor.TransactionAttribute}
+ *
+ * most important base class
+ * {@link org.springframework.transaction.support.AbstractPlatformTransactionManager}
+ *
+ *
  */
 public class TransactionConfig {
 }
