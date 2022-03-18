f0803f15fd3af729bf726073899857dc385cfc4a
jun.liu <jun.liu@dmall.com>
2022-03-18 10:24:12 +0800

    integer parse int

--- demoProject/demo/src/test/java/com/luois/Test.java
+++ demoProject/demo/src/test/java/com/luois/Test.java
@@ -3,6 +3,9 @@ package com.luois;
 import reactor.util.function.Tuple2;
 import reactor.util.function.Tuples;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -15,9 +18,56 @@ import java.util.function.Supplier;
 public class Test {
 
     public static void main(String[] args) {
-        do{
-            System.out.println("-------------死循环e----------------");
-        }while(true);
+        String var="-12399999";
+
+        Map<String,Integer> strMapInt=new HashMap<>();
+        strMapInt.put("0",0);
+        strMapInt.put("1",1);
+        strMapInt.put("2",2);
+        strMapInt.put("3",3);
+        strMapInt.put("4",4);
+        strMapInt.put("5",5);
+        strMapInt.put("6",6);
+        strMapInt.put("7",7);
+        strMapInt.put("8",8);
+        strMapInt.put("9",9);
+        String[] strArr=var.split("");
+
+        int length=strArr.length;
+        Integer result=0;
+        boolean isDeg = false;
+
+        for(int i=0;i<length;i++){
+
+            if (i == 0 && strArr[0].equals("-")) {
+                isDeg = true;
+                continue;
+            }
+
+            Integer itemInt=strMapInt.get(strArr[i]);
+
+            if (Objects.isNull(itemInt)) {
+                throw new NumberFormatException("int format error:" + var);
+            }
+
+            result = (result + itemInt);
+            if (i != length - 1) {
+                result = result * 10;
+            }
+        }
+        if (isDeg) {
+            if (result < Integer.MIN_VALUE) {
+                throw new RuntimeException("result smaller than max integer");
+            }
+            result = -result;
+        } else {
+            if (result > Integer.MAX_VALUE) {
+                throw new RuntimeException("result bigger than max integer");
+            }
+        }
+        System.out.print(result);
+
+
     }
 
 
