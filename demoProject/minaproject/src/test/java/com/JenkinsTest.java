6792a4abce649939d850dabd0fbb6c6f2167fb7d
liujun <liujun@baijiayun.com>
2022-06-21 21:31:11 +0800

    java 获取git 仓库

--- demoProject/minaproject/src/test/java/com/JenkinsTest.java
+++ demoProject/minaproject/src/test/java/com/JenkinsTest.java
@@ -1,6 +1,7 @@
 package com;
 
 import com.cdancy.jenkins.rest.JenkinsClient;
+import com.cdancy.jenkins.rest.domain.common.IntegerResponse;
 import com.cdancy.jenkins.rest.domain.job.Job;
 import com.cdancy.jenkins.rest.domain.job.JobInfo;
 import com.cdancy.jenkins.rest.domain.job.JobList;
@@ -9,16 +10,30 @@ import com.cdancy.jenkins.rest.domain.system.SystemInfo;
 import com.cdancy.jenkins.rest.features.JobsApi;
 import com.cdancy.jenkins.rest.features.PluginManagerApi;
 import com.cdancy.jenkins.rest.features.QueueApi;
+import com.cdancy.jenkins.rest.parsers.FolderPathParser;
+import com.google.common.collect.Maps;
+import com.louis.http.HttpClientUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.util.Lists;
 import org.gitlab.api.GitlabAPI;
 import org.junit.Test;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Objects;
 
 /**
  * @author louis
  * @date 2022/6/20
+ * 1. 获取gitlab 仓库信息
+ * 2. 配置 当前服务的git 仓库
+ * 3. 拉取当前仓库的分支信息
+ * 4. 将分支信息传给jenkins
+ * ========
+ * 2. 获取j job 列表（其实不需要，查看自己有权限项目，将项目与Jenkins job 关系做一个映射）
+ * 3. 构建job 传参
+ *
+ *
  */
 @Slf4j
 public class JenkinsTest {
@@ -33,6 +48,9 @@ public class JenkinsTest {
         JenkinsClient jenkinsClient = test.jenkinsClient();
 
         return jenkinsClient.api().jobsApi();
+    }
+    public static void viewList() {
+
     }
 
     public static PluginManagerApi pluginManagerApi() {
@@ -63,7 +81,7 @@ public class JenkinsTest {
 
         JobsApi jobsApi = jenkinsClient.api().jobsApi();
 
-        JobList dev = jobsApi.jobList("");
+        JobList dev = jobsApi.jobList("dev");
         if (Objects.nonNull(dev)) {
             List<Job> jobs = dev.jobs();
 
@@ -72,6 +90,9 @@ public class JenkinsTest {
             }
         }
     }
+
+
+
     @Test
     public void jobInfo() {
         JenkinsTest test = new JenkinsTest();
@@ -88,8 +109,11 @@ public class JenkinsTest {
 
     @Test
     public void buildJob() {
-        Plugins plugins = pluginManagerApi().plugins(3, null);
-        System.out.println(plugins);
+        Map<String, List<String>> param = Maps.newHashMap();
+        param.put("branch", Lists.newArrayList("develop"));
+        param.put("mvnCommand", Lists.newArrayList("mvn clean package -pl scrm-job -am -DskipTests=true -Pdev -U"));
+        IntegerResponse integerResponse = api().buildWithParameters("", "dev-scrm-job", param);
+        System.out.println(integerResponse);
     }
 
     @Test
@@ -99,7 +123,7 @@ public class JenkinsTest {
     public JenkinsClient jenkinsClient() {
         JenkinsClient client = JenkinsClient.builder()
                 .endPoint("") // Optional. Defaults to http://127.0.0.1:8080
-                .credentials("sdf") // Optional.
+                .credentials("") // Optional.
                 .build();
 
         SystemInfo systemInfo = client.api().systemApi().systemInfo();
