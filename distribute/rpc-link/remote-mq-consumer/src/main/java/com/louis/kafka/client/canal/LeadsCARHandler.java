5d0ea6b7556cd1ff6e64d40eba342e1ef22b9e77
liujun <liujun@baijiayun.com>
2022-08-14 12:46:32 +0800

    init canal

--- distribute/rpc-link/remote-mq-consumer/src/main/java/com/louis/kafka/client/canal/LeadsCARHandler.java
+++ distribute/rpc-link/remote-mq-consumer/src/main/java/com/louis/kafka/client/canal/LeadsCARHandler.java
@@ -1,10 +1,6 @@
 package com.louis.kafka.client.canal;
 
-import com.alibaba.fastjson.JSON;
-import com.alibaba.fastjson.TypeReference;
-import com.alibaba.fastjson.serializer.SerializerFeature;
 import com.louis.kafka.client.annotation.CanalDataFetchHandle;
-import com.louis.kafka.client.canal.AbstractCanalFetchHandler;
 import com.louis.kafka.client.entity.LeadsChannelAllocRules;
 import com.louis.kafka.client.meta.CanalMessage;
 import lombok.extern.slf4j.Slf4j;
@@ -17,16 +13,20 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 @CanalDataFetchHandle(tableName = "leads_channel_alloc_rules")
-public class LeadsCARHandler extends AbstractCanalFetchHandler {
+public class LeadsCARHandler extends AbstractCanalFetchHandler<LeadsChannelAllocRules> {
 
     @Override
-    public void handle(String msg) {
+    public void insert(CanalMessage<LeadsChannelAllocRules> leadsChannelAllocRules) {
 
-        CanalMessage<LeadsChannelAllocRules> canalMessage = JSON.parseObject(msg, CanalMessage.class);
+    }
 
-        log.info("canalMessage:{}", JSON.toJSONString(canalMessage, SerializerFeature.PrettyFormat));
+    @Override
+    public void update(CanalMessage<LeadsChannelAllocRules> leadsChannelAllocRules) {
 
+    }
 
+    @Override
+    public void delete(CanalMessage<LeadsChannelAllocRules> leadsChannelAllocRules) {
 
     }
 }
