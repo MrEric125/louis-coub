d4c7fd9835567a720fa3366079c1275759b22246
louis <627460581@qq.com>
2023-12-08 09:35:27 +0800

    rabitMQ test

---
+++ rabitmq/consumer/src/main/java/org/example/RabitMQConfig.java
@@ -0,0 +1,27 @@
+package org.example;
+
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+@Configuration
+public class RabitMQConfig {
+
+
+
+    @Bean
+    public Connection connect() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setUsername("guest");
+        factory.setPassword("guest");
+        factory.setVirtualHost("/");
+        factory.setHost("localhost");
+        factory.setPort(5672);
+        return factory.newConnection();
+    }
+
+}
