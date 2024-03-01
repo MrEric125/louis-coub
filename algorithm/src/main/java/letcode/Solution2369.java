21d789f7c654c1ff9a4d1d2e2e6cd4ed1688b1ae
louis <627460581@qq.com>
2024-03-01 15:49:51 +0800

    算法题目

---
+++ algorithm/src/main/java/letcode/Solution2369.java
@@ -0,0 +1,11 @@
+package letcode;
+
+/**
+ * https://leetcode.cn/problems/check-if-there-is-a-valid-partition-for-the-array/description/
+ * 动态规划解法：
+ * 1. 穷举法
+ * 2. 记忆化搜索/缓存
+ * 3. 改写成迭代算法
+ */
+public class Solution2369 {
+}
---
+++ algorithm/src/main/java/letcode/Solution70.java
@@ -0,0 +1,63 @@
+package letcode;
+
+import com.google.common.collect.Maps;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+public class Solution70 {
+
+    public int climbStairs(int n) {
+        if (n == 1) {
+            return 1;
+        }
+        if (n == 2) {
+            return 2;
+        }
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+    public int climbStairs2(int n) {
+        Map<Integer, Integer> calculate = Maps.newHashMap();
+
+        int result = climbStairsByCache(n, calculate);
+
+        System.out.println(calculate);
+
+        return result;
+    }
+
+    public int climbStairsByCache(int n, Map<Integer, Integer> memo) {
+        Integer result = memo.get(n);
+        if (Objects.nonNull(result)) {
+            return result;
+        }
+
+        if (n == 1) {
+            memo.put(1, 1);
+            return 1;
+        }
+        if (n == 2) {
+            memo.put(2, 2);
+            return 2;
+        }
+        int result1 = climbStairsByCache(n - 1, memo);
+        memo.put(n - 1, result1);
+        int result2 = climbStairsByCache(n - 2, memo);
+        memo.put(n - 2, result1);
+        return result2 + result1;
+    }
+
+    public static void main(String[] args) {
+        Solution70 solution70 = new Solution70();
+        long s1 = System.currentTimeMillis();
+
+        System.out.println(solution70.climbStairs(45));
+        long r1 = System.currentTimeMillis();
+        System.out.println("耗時：" + (r1 - s1));
+        long s2 = System.currentTimeMillis();
+        System.out.println(solution70.climbStairs2(45));
+        long r2 = System.currentTimeMillis();
+        System.out.println("耗時：" + (r2 - s2));
+    }
+}
