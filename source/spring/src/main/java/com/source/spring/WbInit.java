95167c15402ddb3b96ed694ddf687a6394754ef9
jun.liu <jun.liu@dmall.com>
2024-10-28 16:22:58 +0800

    build project

--- source/spring/src/main/java/com/source/spring/WbInit.java
+++ source/spring/src/main/java/com/source/spring/WbInit.java
@@ -1,23 +1,16 @@
 package com.source.spring;
 
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletException;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.WebApplicationInitializer;
-import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
-import org.springframework.web.servlet.DispatcherServlet;
-
-import javax.servlet.ServletContext;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRegistration;
 
 @ComponentScan(basePackages = "com.source")
 public class WbInit implements WebApplicationInitializer {
+
+
+    @Override
     public void onStartup(ServletContext servletContext) throws ServletException {
-//        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
-//        ctx.register(MvcConfig.class);
-//        ctx.setServletContext(servletContext);
-//        ServletRegistration.Dynamic dynamic = servletContext.addServlet("dispatcher",new DispatcherServlet(ctx));
-//        dynamic.addMapping("/");
-//        dynamic.setLoadOnStartup(1);
-//
+
     }
 }
